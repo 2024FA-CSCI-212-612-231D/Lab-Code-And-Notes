5a5b90c85bbad52439d738fce997ff751dfc04b3
Aleksandra Georgievska <76499696+aleksgeorgi@users.noreply.github.com>
2024-09-02 21:49:22 -0400

    problems posted for lab2

---
+++ src/labproblems/lab2/Lab2SumTheDigits.java
@@ -0,0 +1,9 @@
+package labproblems.lab2;
+
+public class Lab2SumTheDigits {
+	// Lab Instructions:
+	// Write a static method that has an integer as a parameter and returns the sum of the digits in that number. Then write code in a main method to test it.
+
+	// TODO Lab Solution:
+	
+}
