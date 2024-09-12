0762b0e930b4405c9968d1f7871945bf508cb573
Aleksandra Georgievska <76499696+aleksgeorgi@users.noreply.github.com>
2024-09-12 18:44:44 -0400

    adding easy solution

---
+++ src/labsolutions/lab5/Lab5ArraysEasy_FirstLast6Solution.java
@@ -0,0 +1,18 @@
+package labsolutions.lab5;
+
+public class Lab5ArraysEasy_FirstLast6Solution {
+
+	public static boolean firstLast6(int[] nums) {
+		if (nums[0] == 6 || nums[nums.length-1]==6){
+			return true; 
+		} else {
+			return false;
+		}
+	}
+	
+	public static void main (String[] args) {
+		System.out.println(firstLast6(new int[]{1, 2, 6})); // true
+		System.out.println(firstLast6(new int[]{6, 1, 2, 3})); // true 
+		System.out.println(firstLast6(new int[]{13, 6, 1, 2, 3})); // false
+	}
+}
