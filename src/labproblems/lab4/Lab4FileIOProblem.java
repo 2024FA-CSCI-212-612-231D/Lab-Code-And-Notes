5a96040f60251a47f03ec27498024785c7b0e4ff
Aleksandra Georgievska <76499696+aleksgeorgi@users.noreply.github.com>
2024-09-12 14:54:53 -0400

    lab4 problems

--- src/labproblems/lab4/Lab4FileIOProblem.java
+++ src/labproblems/lab4/Lab4FileIOProblem.java
@@ -10,14 +10,14 @@ public class Lab4FileIOProblem {
 	 * Lab Problem:
 	 * 
 	 * Run the FileIO class using your own input file. 
-	 * (HINT: you need to replace the "inputFile.txt" with a real file pathway on your computer)
+	 * (HINT: you need to replace the "inputFile.txt" with a real file pathway)
 	 * */
 
 	public static void main(String[] args) {
 		//try-catch statement attempts to run code and avoids errors
 		//try-catch (or throws) required for checked exceptions such as FileNotFoundException
 		//this is try-with-resources
-		try(Scanner input = new Scanner(new File("inputFile.txt"));
+		try(Scanner input = new Scanner(new File("inputFile.txt")); 
 				PrintWriter output = new PrintWriter(new File("outputFile.txt"))) {
 			while(input.hasNext()) {
 				System.out.println(input.next());
