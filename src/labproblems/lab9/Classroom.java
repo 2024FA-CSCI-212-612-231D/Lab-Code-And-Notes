a9f56fc74098206744268249fa389f9eff3a9679
Aleksandra Georgievska <76499696+aleksgeorgi@users.noreply.github.com>
2024-10-01 13:15:06 -0400

    adding lab9 problem

---
+++ src/labproblems/lab9/Classroom.java
@@ -0,0 +1,31 @@
+package labproblems.lab9;
+
+public class Classroom {
+	/* Write a class that represents a classroom. 
+	 * A classroom has 
+	 *     a room number, 
+	 *     length, 
+	 *     width, 
+	 *     lights that are either on or off, 
+	 *     and a door that is either open or closed. 
+	 *     
+	 * Create all appropriate 
+	 *     private instance variables, 
+	 * 	   add a constructor, 
+	 *     accessor methods, 
+	 *     a method that returns the square footage of the room, 
+	 *     methods to flip the light switch and open 
+	 *     or shut the door and a toString method. 
+	 * 
+	 * Test it using a main method.
+	 * 
+	 * */
+	
+	
+	
+	public static void main (String[] args) {
+		// test your classroom class here
+		
+	}
+
+}
