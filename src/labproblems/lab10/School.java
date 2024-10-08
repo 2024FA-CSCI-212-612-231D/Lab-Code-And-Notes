33b25ea454cab6d901f71962eb36d1e94284d314
Aleksandra Georgievska <76499696+aleksgeorgi@users.noreply.github.com>
2024-10-08 11:55:32 -0400

    adding lab 10 problems

---
+++ src/labproblems/lab10/School.java
@@ -0,0 +1,35 @@
+package labproblems.lab10;
+
+import labsolutions.lab9.Classroom;
+
+public class School {
+	
+	// declare a private field called 'rooms'
+	// to store an array of classrooms
+	// TODO
+	
+	// create a class constructor that takes an array
+	// of Classroom objects called 'rooms' and
+	// initializes the room field of this class instance
+	// TODO
+	
+	// create a public method called 'getLargestRoom' that 
+	// takes no parameters but returns a Classroom object 
+	// of the largest room in the 'rooms' array
+	// TODO
+
+	// create a public method called countRoomsWithLightsOn
+	// that takes no parameters but returns a count
+	// of the number of classroom rooms that has lights on
+	// TODO
+
+	// create a public method called 'closeAllDoors' that
+	// takes no parameters and returns nothing. It simply
+	// closes the doors for all the rooms in the school
+	// (ie the Classroom array)
+	// TODO
+	
+	// NOTE you do not need to write a main method
+	// for this School class to test your methods
+
+}
