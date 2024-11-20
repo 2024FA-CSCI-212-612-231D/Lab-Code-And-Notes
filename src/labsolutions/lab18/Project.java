c9319ce57f163a7edc6fe6612f50029c7a3cd116
Aleksandra Georgievska <76499696+aleksgeorgi@users.noreply.github.com>
2024-11-19 19:23:03 -0500

    adding lab 18 solutions

---
+++ src/labsolutions/lab18/Project.java
@@ -0,0 +1,77 @@
+package labsolutions.lab18;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Project {
+	
+	private String projectName;
+	private List<Component> components = new ArrayList<Component>();
+	
+	public Project(String name) {
+		projectName = name;
+	}
+	
+	public void addComponent(ComponentType type) {
+		components.add(new Component(type, type.manufacture()));
+	}
+	
+	public int getNumComponents() {
+		return components.size();
+	}
+	
+	public Component getComponent(int num) {
+		return components.get(num);
+	}
+	
+	public double getTotalPrice() {
+		double price = 0;
+		for(Component component : components) {
+			price += component.type.price;
+		}
+		return price;
+	}
+	
+	public static class ComponentType{
+		
+		private final String componentName;
+		private int numberManufactured;
+		private final double price;
+		
+		public ComponentType(String componentName, double price) {
+			this.componentName = componentName;
+			this.price = price;
+		}
+		
+		public String getName() {
+			return componentName;
+		}
+		
+		public double getPrice() {
+			return price;
+		}
+		
+		private int manufacture() {
+			numberManufactured++;
+			return numberManufactured;
+		}
+		
+	}
+	
+	public class Component{
+		
+		private final ComponentType type;
+		private final int manufactureNum;
+		
+		private Component(ComponentType type, int num) {
+			this.type = type;
+			manufactureNum = num;
+		}
+		
+		public String toString() {
+			return projectName + ": " + type.getName() + " #" + manufactureNum + ", $" + type.getPrice();
+		}
+		
+	}
+
+}
